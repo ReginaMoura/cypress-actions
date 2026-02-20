15b8551e049add4ad2b8de38672912cec8f05875
ReginaMoura <76976582+ReginaMoura@users.noreply.github.com>
2026-02-20 19:47:43 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'u5x39s',
+  projectId: '96n3qy',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
